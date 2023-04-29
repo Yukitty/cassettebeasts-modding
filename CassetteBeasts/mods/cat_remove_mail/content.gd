ab1ddb623e5ad1c0d87c4e49b57a03a04137d4df
Yukita Mayako <catgirl@goddess.moe>
2023-04-29 03:04:46 -0400

    Add No Café Mail mod
    Procedurally removes the InterdimensionalPostBox from
    the Gramophone Café every time it loads.

---
+++ CassetteBeasts/mods/cat_remove_mail/content.gd
@@ -0,0 +1,12 @@
+extends ContentInfo
+
+func init_content() -> void:
+	TranslationServer.add_translation(preload("res://mods/cat_remove_mail/mod_strings.en.translation"))
+	DLC.get_tree().connect("node_added", self, "_on_node_added", [], CONNECT_DEFERRED)
+
+func _on_node_added(node: Node) -> void:
+	if node.filename == "res://world/maps/interiors/GramophoneInterior.tscn":
+		var post_box: Node
+		post_box = node.get_node("ExpoConditionalLayer/InterdimensionalPostBox")
+		if post_box:
+			post_box.queue_free()
