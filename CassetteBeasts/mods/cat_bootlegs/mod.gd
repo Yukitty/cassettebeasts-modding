5fec361f303255bf13c69f59ef2b1622499763c8
Yukita Mayako <catgirl@goddess.moe>
2023-05-06 03:57:57 -0400

    Update modutils const

--- CassetteBeasts/mods/cat_bootlegs/mod.gd
+++ CassetteBeasts/mods/cat_bootlegs/mod.gd
@@ -16,17 +16,21 @@ const RESOURCES: Dictionary = {
 var setting_bootleg_chance: float = 0.1
 
 # Mod interoperability
-const modutils_settings = [
-	{
-		"property": "setting_bootleg_chance",
-		"type": "slider",
-		"label": "Overworld Bootleg Chance",
-		"min_value": 0.1,
-		"max_value": 10.0,
-		"step": 0.1,
-		"format": "{value}%",
-	}
-]
+const MODUTILS = {
+	"cheat_mod": true,
+
+	"settings": [
+		{
+			"property": "setting_bootleg_chance",
+			"type": "slider",
+			"label": "Overworld Bootleg Chance",
+			"min_value": 0.1,
+			"max_value": 10.0,
+			"step": 0.1,
+			"format": "{value}%",
+		}
+	]
+}
 
 func _init() -> void:
 	for translation in TRANSLATIONS:
